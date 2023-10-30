8a2d35b8b653f521bfd86a972f14ef96684ee288
Aletsandr <a.i.kuznetsov.93@mail.ru>
2023-10-30 19:23:20 +0300

    My one commit from local PC

---
+++ bd/ddl-init.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS Authors(	--авторы
+	id SERIAL primary key,
+	author_№ varchar (255),  --номер билета
+	book_№ varchar (255),
+	name_author varchar (255)
+);
+
+CREATE TABLE IF NOT EXISTS Publishing(	--идательство
+	id SERIAL primary key,
+	title_publishing varchar (255),  --название идательства
+	address varchar (255),
+	year_publication date
+);
+
+CREATE TABLE IF NOT EXISTS Books( --книги	
+	id SERIAL primary key,
+	title varchar (255), -- название
+	number_of_pages varchar (255), -- кол-во страниц
+	price decimal(10,2), --цена
+	number_of_instances varchar (255), -- кол-во экземпляров
+	genre varchar (255),    -- жанр
+	Authors_id bigint, -- авторы
+	foreign key (Authors_id)references Authors (id) on delete cascade,
+	Publishing_id bigint,  -- издательство
+	foreign key (Publishing_id)references Publishing (id) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS Readers(	--читатели
+	id SERIAL primary key,
+	ticket_№ varchar (255),  --номер билета
+	FIO varchar (255),
+	address varchar (255),
+	telephone_№ varchar (255)
+);
+
+CREATE TABLE IF NOT EXISTS Workers(	--работники
+	id SERIAL primary key,
+	FIO varchar (255)
+);
+
+CREATE TABLE IF NOT EXISTS Extradition( --выдача	
+	Extradition_id SERIAL primary key,
+	start_date date,
+	return_date date,
+	actual_refund date, --фактичсекий возврат
+	Books_id bigint,
+	foreign key (Books_id)references Books (id) on delete cascade,
+	Workers_id bigint,
+	foreign key (Workers_id)references Workers (id) on delete cascade,
+	Readers_id bigint,
+	foreign key (Readers_id)references Readers (id) on delete cascade
+);
